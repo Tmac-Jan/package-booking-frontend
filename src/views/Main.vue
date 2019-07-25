fe330db28076ed2fd8f69f2f32f396a4f7aa0de0
zhanggi3 <gio.zhang@oocl.com>
2019-07-25 22:33:49 +0800

    [refactor]:[delete dead code on mutations-type.js,rename Main]

---
+++ src/views/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Welcome/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Welcome from '@/components/Main.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Welcome
+  }
+}
+</script>
